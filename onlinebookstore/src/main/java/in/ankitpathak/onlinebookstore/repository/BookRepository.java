0fe6a529a6755fb9f8efdc9c6579514f265c14c5
Ankit Kumar Pathak <Ankit.Pathak@startufparms.com>
2020-08-02 19:00:09 +0530

    Created Angular project and completed the feature 1

--- onlinebookstore/src/main/java/in/ankitpathak/onlinebookstore/repository/BookRepository.java
+++ onlinebookstore/src/main/java/in/ankitpathak/onlinebookstore/repository/BookRepository.java
@@ -4,6 +4,7 @@
 package in.ankitpathak.onlinebookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import in.ankitpathak.onlinebookstore.entity.Book;
 
@@ -11,6 +12,8 @@ import in.ankitpathak.onlinebookstore.entity.Book;
  * @author Ankit.Pathak
  *
  */
+
+@CrossOrigin("http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book, Long> {
 
 }
